c87c2836939b40fb1e145e3501d7fafc2edf49af
praynext <2068525908@qq.com>
2022-10-19 14:50:06 +0800

    jfkafklsdafhlkadsh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home/home';
 import Follow from './pages/follow/Follow'
 import Write from './pages/write/Write'
 import Login from './pages/login/Login'
+import WrappedRegistrationForm from './pages/login/register'
 import {
     BrowserRouter as Router,
     Route,
@@ -14,12 +15,13 @@ import {
 function App() {
     return (
         <Router>
-            <Navbar/>
+            <Navbar />
             <Routes>
-                <Route exact path="/" element={<Home/>}/>
-                <Route exact path="/follow" element={<Follow/>}/>
-                <Route exact path="/write" element={<Write/>}/>
-                <Route exact path="/login" element={<Login/>}/>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/follow" element={<Follow />} />
+                <Route exact path="/write" element={<Write />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path='/register' element={<WrappedRegistrationForm/>} />
             </Routes>
         </Router>
     );
