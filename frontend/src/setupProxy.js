002ab4ca2c6c9a60339fafe13ab0105f9ba81ddf
praynext <2068525908@qq.com>
2022-11-07 23:12:30 +0800

    fhakfhak

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://localhost:8000',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': '',
+            }
+        })
+    );
+};
