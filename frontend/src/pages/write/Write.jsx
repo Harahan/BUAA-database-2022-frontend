5f72346cd24cc80b29d483758581858e0f645ce3
陈紫莹 <peaceminuczy@163.com>
2022-11-11 22:44:29 +0800

    write-page

--- frontend/src/pages/write/Write.jsx
+++ frontend/src/pages/write/Write.jsx
@@ -1,57 +1,137 @@
-import * as React from "react"
-import ReactMde from "react-mde"
-import ReactDOM from "react-dom"
-import { Typography, Tag } from "antd"
-import * as Showdown from "showdown"
-import "react-mde/lib/styles/css/react-mde-all.css"
-import "./write.css"
+import { useState, useEffect, useRef } from 'react';
+import { Typography, Input, Tag, Tooltip, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { i18nChangeLanguage } from '@wangeditor/editor'
+import { Editor, Toolbar } from '@wangeditor/editor-for-react'
+import '@wangeditor/editor/dist/css/style.css'
+import axios from 'axios'
+import './write.css'
 
+const Write = () => {
+    const [ title, setTitle ] = useState( 'Title' );
+    const [ editor, setEditor ] = useState( null );
+    const [ tags, setTags ] = useState( [ 'some tag' ] );
+    const [ inputVisible, setInputVisible ] = useState( false );
+    const [ inputValue, setInputValue ] = useState( '' );
+    const [ editInputIndex, setEditInputIndex ] = useState( -1 );
+    const [ editInputValue, setEditInputValue ] = useState( '' );
+    const [ loading, setLoading ] = useState( false );
+    const [ firstPic, setfirstPic ] = useState( null );
+    const [ html, setHtml ] = useState( '' );
+    const inputRef = useRef( null );
+    const editInputRef = useRef( null );
 
-const converter = new Showdown.Converter( {
-    tables: true,
-    simplifiedAutoLink: true,
-    strikethrough: true,
-    tasklists: true
-} );
+    i18nChangeLanguage( 'en' )
 
-export default function Write () {
-
-    const [ value, setValue ] = React.useState( "**Hello world!!!**" );
-    const [ selectedTab, setSelectedTab ] = React.useState( "write" );
-    const [ title, setTitle ] = React.useState( 'Title' );
-    const { CheckableTag } = Tag;
-    const tagsData = [ 'Movies', 'Books', 'Music', 'Sports' ];
-    const [ selectedTags, setSelectedTags ] = React.useState( [ 'Books' ] );
-    const handleChange = ( tag, checked ) => {
-        const nextSelectedTags = checked
-            ? [ ...selectedTags, tag ]
-            : selectedTags.filter( ( t ) => t !== tag );
-        console.log( 'You are interested in: ', nextSelectedTags );
-        setSelectedTags( nextSelectedTags );
+    const postArticle = () => {
+        setLoading( true );
+        console.log( html );
+        let formData = new FormData();
+        formData.append( "html", html );
+        formData.append( "tags", tags );
+        formData.append( "title", title );
+        if ( firstPic ) {
+            formData.append( "cover", firstPic );
+        }
+        axios(
+            {
+                url: "http://localhost:3000/api/postArticle",
+                method: "POST",
+                data: formData,
+            }
+        ).then( ( res ) => {
+            setLoading( false );
+        }
+        )
     };
 
-    const save = async function* ( data ) {
-        const wait = function ( time ) {
-            return new Promise( ( a, r ) => {
-                setTimeout( () => a(), time );
-            } );
-        };
+    const toolbarConfig = {}
+    const editorConfig = {
+        placeholder: 'write your blog...',
+        MENU_CONF: {}
+    }
 
-        await wait( 2000 );
-        yield "https://picsum.photos/300";/*TODO 换成获取返回图片的链接*/
-        await wait( 2000 );
-        return true;
+    editorConfig.MENU_CONF[ 'uploadImage' ] = {
+        async customUpload ( file, insertFn ) {
+            let formData = new FormData();
+            formData.append( "file", file );
+            axios(
+                {
+                    url: "http://localhost:3000/api/upload",
+                    method: "GET",
+                    data: formData,
+                }
+            ).then(
+                ( res ) => {
+                    insertFn( res.data.url )
+                    if ( firstPic === null ) {
+                        setfirstPic( res );
+                        console.log( res );
+                    }
+                }
+            )
+        },
+        onBeforeUpload ( file ) {
+            return file
+        },
+        onProgress ( progress ) {
+            console.log( 'progress', progress )
+        },
+        onSuccess ( file, res ) {
+            console.log( `${ file.name } succeed`, res )
+        },
+        onFailed ( file, res ) {
+            console.log( `${ file.name } failed`, res )
+        },
+        onError ( file, err, res ) {
+            console.log( `${ file.name } error`, err, res )
+        }
+    }
+
+    useEffect( () => {
+        if ( inputVisible ) {
+            inputRef.current?.focus();
+        }
+    }, [ inputVisible ] );
+    useEffect( () => {
+        editInputRef.current?.focus();
+    }, [ inputValue ] );
+    const handleClose = ( removedTag ) => {
+        const newTags = tags.filter( ( tag ) => tag !== removedTag );
+        console.log( newTags );
+        setTags( newTags );
+    };
+    const showInput = () => {
+        setInputVisible( true );
+    };
+    const handleInputChange = ( e ) => {
+        setInputValue( e.target.value );
+    };
+    const handleInputConfirm = () => {
+        if ( inputValue && tags.indexOf( inputValue ) === -1 ) {
+            setTags( [ ...tags, inputValue ] );
+        }
+        setInputVisible( false );
+        setInputValue( '' );
+    };
+    const handleEditInputChange = ( e ) => {
+        setEditInputValue( e.target.value );
+    };
+    const handleEditInputConfirm = () => {
+        const newTags = [ ...tags ];
+        newTags[ editInputIndex ] = editInputValue;
+        setTags( newTags );
+        setEditInputIndex( -1 );
+        setInputValue( '' );
     };
 
     return (
-        <div className="writePage">
-            <div className="writeTitle">
+        <div className="Write">
+            <div className="BlogTitle">
                 <Typography.Title
                     editable={ {
-                        tooltip: false,
                         onChange: setTitle,
                     } }
-                    level={ 1 }
                     style={ {
                         margin: 0,
                     } }
@@ -59,36 +139,102 @@ export default function Write () {
                     { title }
                 </Typography.Title>
             </div>
-            <div className="writeTags">
-                { tagsData.map( ( tag ) => (
-                    <CheckableTag
-                        key={ tag }
-                        checked={ selectedTags.indexOf( tag ) > -1 }
-                        onChange={ ( checked ) => handleChange( tag, checked ) }
-                    >
-                        { tag }
-                    </CheckableTag>
-                ) ) }
-            </div>
-            <div className="writeArea">
-                <ReactMde
-                    value={ value }
-                    onChange={ setValue }
-                    selectedTab={ selectedTab }
-                    onTabChange={ setSelectedTab }
-                    generateMarkdownPreview={ markdown =>
-                        Promise.resolve( converter.makeHtml( markdown ) )
+            <div className="BlogTag">
+                { tags.map( ( tag, index ) => {
+                    if ( editInputIndex === index ) {
+                        return (
+                            <Input
+                                ref={ editInputRef }
+                                key={ tag }
+                                size="small"
+                                className="tag-input"
+                                value={ editInputValue }
+                                onChange={ handleEditInputChange }
+                                onBlur={ handleEditInputConfirm }
+                                onPressEnter={ handleEditInputConfirm }
+                            />
+                        );
                     }
-                    childProps={ {
-                        writeButton: {
-                            tabIndex: -1
-                        }
-                    } }
-                    paste={ {
-                        saveImage: save
-                    } }
+                    const isLongTag = tag.length > 20;
+                    const tagElem = (
+                        <Tag
+                            className="edit-tag"
+                            key={ tag }
+                            closable={ true }
+                            onClose={ () => handleClose( tag ) }
+                        >
+                            <span
+                                onDoubleClick={ ( e ) => {
+                                    if ( index !== 0 ) {
+                                        setEditInputIndex( index );
+                                        setEditInputValue( tag );
+                                        e.preventDefault();
+                                    }
+                                } }
+                            >
+                                { isLongTag ? `${ tag.slice( 0, 20 ) }...` : tag }
+                            </span>
+                        </Tag>
+                    );
+                    return isLongTag ? (
+                        <Tooltip title={ tag } key={ tag }>
+                            { tagElem }
+                        </Tooltip>
+                    ) : (
+                        tagElem
+                    );
+                } ) }
+                { inputVisible && (
+                    <Input
+                        ref={ inputRef }
+                        type="text"
+                        size="small"
+                        className="tag-input"
+                        value={ inputValue }
+                        onChange={ handleInputChange }
+                        onBlur={ handleInputConfirm }
+                        onPressEnter={ handleInputConfirm }
+                    />
+                ) }
+                { !inputVisible && (
+                    <Tag className="site-tag-plus" onClick={ showInput }>
+                        <PlusOutlined /> New Tag
+                    </Tag>
+                ) }
+            </div>
+            <div className="BlogEditor">
+                <div style={ { border: '1px solid #ccc', zIndex: 100, marginTop: '15px' } }>
+                    <Toolbar
+                        editor={ editor }
+                        defaultConfig={ toolbarConfig }
+                        mode="default"
+                        style={ { borderBottom: '1px solid #ccc' } }
+                    />
+                    <Editor
+                        defaultConfig={ editorConfig }
+                        value={ html }
+                        onCreated={ setEditor }
+                        onChange={ editor => setHtml( editor.getHtml() ) }
+                        mode="default"
+                        style={ { height: '500px' } }
+                    />
+                </div>
+                <div className="editor-content-view"
+                    dangerouslySetInnerHTML={ { __html: html } }
                 />
             </div>
+            <div className="PostButton">
+                <Button
+                    size={ "large" }
+                    loading={ loading }
+                    onClick={ () => postArticle() }
+                >
+                    POST
+                </Button>
+            </div>
         </div>
-    )
+
+    );
 }
+
+export default Write;
