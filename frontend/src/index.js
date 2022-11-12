1b93136863d15feca2082b2672129efb918c3f1d
陈紫莹 <peaceminuczy@163.com>
2022-11-12 16:27:16 +0800

    chat

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from './App'
+import { ChatContextProvider } from "./components/ChatContext/ChatContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <ChatContextProvider>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    </ChatContextProvider>
 );
--- frontend/src/pages/follow/Follow.jsx
+++ frontend/src/pages/follow/Follow.jsx
@@ -1,7 +1,46 @@
 import './follow.css'
+import { Tabs } from 'antd'
+import { BookOutlined, ShoppingCartOutlined, BulbOutlined } from '@ant-design/icons'
 
 export default function Follow () {
     return (
-        <div>Follow</div>
+        <div className="Follow">
+            <Tabs
+                defaultActiveKey="1"
+                tabPosition={ "left" }
+                items={ [
+                    {
+                        label: (
+                            <span>
+                                <BookOutlined />
+                                Blog
+                            </span>
+                        ),
+                        key: '1',
+                        children: `Content of Tab Pane 1`,
+                    },
+                    {
+                        label: (
+                            <span>
+                                <ShoppingCartOutlined />
+                                Releases
+                            </span>
+                        ),
+                        key: '2',
+                        children: `Content of Tab Pane 2`,
+                    },
+                    {
+                        label: (
+                            <span>
+                                <BulbOutlined />
+                                Idea
+                            </span>
+                        ),
+                        key: '3',
+                        children: `Content of Tab Pane 3`,
+                    },
+                ] }
+            />
+        </div>
     )
 }
