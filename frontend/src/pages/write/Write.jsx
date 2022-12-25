6a6a4a2883498b0c112cb163119f3e0a0ef2ce71
陈紫莹 <peaceminuczy@163.com>
2022-12-25 10:36:45 +0800

    edit fixed

--- frontend/src/pages/write/Write.jsx
+++ frontend/src/pages/write/Write.jsx
@@ -8,7 +8,7 @@ const Write = ( props ) => {
     const params = useParams();
     const [ html_content, setHtml_content ] = useState( "" );
     const [ cover, setCover ] = useState( "" );
-    const [ tags, setTags ] = useState( [ 'Technology', 'Food', 'Music', 'Business', 'MoviesTV', 'Sport' ] );
+    const [ tags, setTags ] = useState( [] );
     useEffect(
         () => {
             fetch( `/api/blog/fetchOne/`, {
@@ -19,7 +19,6 @@ const Write = ( props ) => {
                     tit: params.title
                 } ),
             } ).then( res => res.json() ).then( data => {
-                setTags( data[ 0 ].categories );
                 fetch(
                     data[ 0 ].html.replace( '39.106.5.232', '39.106.5.232:3000/api' ), {
                     method: 'get',
@@ -31,6 +30,7 @@ const Write = ( props ) => {
                         res.text().then( html_data => {
                             article.html = html_data;
                             setCover( article.cover );
+                            setTags( data[ 0 ].categories );
                             setHtml_content( article.html );
                             // console.log( article.html )
                         } )
@@ -45,7 +45,7 @@ const Write = ( props ) => {
                 oriHtml={ params ? html_content : props.oriHtml }
                 oriTitle={ params ? params.title : props.oriTitle }
                 oriCover={ cover }
-                oriTags={ tags }
+                oriTags={ params ? tags : [ 'Technology', 'Food', 'Music', 'Business', 'MoviesTV', 'Sport' ] }
             />
         </div>
     );
